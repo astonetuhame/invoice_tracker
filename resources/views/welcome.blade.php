a67a63a7556de7cfc0cc65a5a1bc667b65d701fd
Tuhame Astone <astonetuhame@gmail.com>
2021-12-08 12:36:02 +0300

    Updated welcome balde file

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,7 +9,7 @@
                             <h1>Sibed Invoice Tracker</h1>
                             <p class="lead">Welcome to Sibed Invoice Tracker, making invoice tracking easier.</p>
                             <a class="btn btn-lg btn-primary" href="{{ route('login') }}" role="button">Please Login to continue</a>
-                            {{-- <a class="btn btn-lg btn-primary" href="{{ route('register') }}" role="button">No Account? Register here</a> --}}
+                            <a class="btn btn-lg btn-primary" href="{{ route('register') }}" role="button">No Account? Register here</a> 
                     </div>
                 </div>
             </div>
